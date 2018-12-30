d5c63af43cc11fde452c15b13954f7f5883f14aa
Kasaract <35552077+Kasaract@users.noreply.github.com>
2018-12-30 16:46:53 -0600

    Updates

---
+++ precache-manifest.793567423ddee0fca44f7f20a82092fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pers-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a920df7d6a589191092f",
+    "url": "/pers-web/static/js/runtime~main.a920df7d.js"
+  },
+  {
+    "revision": "c77c8cc7818af700c9c8",
+    "url": "/pers-web/static/js/main.c77c8cc7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/pers-web/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c77c8cc7818af700c9c8",
+    "url": "/pers-web/static/css/main.ee6ceaaa.chunk.css"
+  },
+  {
+    "revision": "73a1eb33291fe1b4ddf581e0c2eb38a4",
+    "url": "/pers-web/index.html"
+  }
+];
