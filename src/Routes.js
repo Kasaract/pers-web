7f77eebda702dc4338eaff693df88e6ef86ef67e
Kasaract <35552077+Kasaract@users.noreply.github.com>
2019-01-07 18:39:48 -0600

    Added Read to Routes

--- src/Routes.js
+++ src/Routes.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './Home';
-import About from './About';
+import Read from './Read';
 
 const Routes = () => (
     <Router>
         <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="*" component={Error} />
+            <Route exact path="/read" component={Read} />
         </Switch>
     </Router>
 )
