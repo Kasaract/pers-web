8d96fdfafee4a6ded1daae95b9c092b32ba75106
Kasaract <35552077+Kasaract@users.noreply.github.com>
2019-01-07 18:31:14 -0600

    Added empty page for Read

---
+++ src/Read/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import NavBar from '../share/components/NavBar';
+import Footer from '../share/components/Footer';
+import './read.scss';
+
+const Read = () => {
+  return (
+    <div>
+      <NavBar />
+      <Footer />
+    </div>     
+  )
+}
+
+export default Read;
