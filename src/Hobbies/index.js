24d29652172f6840a560da4eab8a7405fc5b27a1
Kasaract <35552077+Kasaract@users.noreply.github.com>
2019-02-04 18:13:23 -0500

    Added and basic styling for Hobbies section of home page

--- src/Hobbies/index.js
+++ src/Hobbies/index.js
@@ -2,28 +2,34 @@ import React from 'react';
 
 import Tagline from './components/Tagline';
 import HobbyItem from './components/HobbyItem';
+import './hobbies.scss';
 
 const Hobbies = () => {
   return (
-    <div>
-      <Tagline
-        text="I'm a learner"
-      />
-      <HobbyItem 
-        icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH3CTaF6mpX4Zm3zv8W0bUbc-R5s77UQaqK8YE_1SVk7i7Wy7E"
-        text="Listen"
-        hobby="/listen"
-      />
-      <HobbyItem 
-        icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH3CTaF6mpX4Zm3zv8W0bUbc-R5s77UQaqK8YE_1SVk7i7Wy7E"
-        text="Listen"
-        hobby="/listen"
-      />
-      <HobbyItem 
-        icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH3CTaF6mpX4Zm3zv8W0bUbc-R5s77UQaqK8YE_1SVk7i7Wy7E"
-        text="Listen"
-        hobby="/listen"
-      />
+    <div className="hobbies__container">
+      <div className="hobbies__headline">Hobbies</div>
+      <div className="hobbies__tagline">
+        <Tagline
+          text="I'm a learner"
+        />
+      </div>
+      <div className="hobbies__items-container">
+        <HobbyItem 
+          icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH3CTaF6mpX4Zm3zv8W0bUbc-R5s77UQaqK8YE_1SVk7i7Wy7E"
+          text="Listen"
+          hobby="/listen"
+        />
+        <HobbyItem 
+          icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH3CTaF6mpX4Zm3zv8W0bUbc-R5s77UQaqK8YE_1SVk7i7Wy7E"
+          text="Listen"
+          hobby="/listen"
+        />
+        <HobbyItem 
+          icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH3CTaF6mpX4Zm3zv8W0bUbc-R5s77UQaqK8YE_1SVk7i7Wy7E"
+          text="Listen"
+          hobby="/listen"
+        />
+      </div>
     </div>
   )
 }
