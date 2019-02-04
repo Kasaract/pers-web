33a1b2667069b7cfa76d66ec510527c592eecbff
Kasaract <35552077+Kasaract@users.noreply.github.com>
2019-02-04 17:13:52 -0500

    Added smooth scrolling and navbar buttons for dev purposes

--- src/share/components/NavBar/index.js
+++ src/share/components/NavBar/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Link, animateScroll as scroll } from 'react-scroll';
+//import { Link, animateScroll as scroll } from 'react-scroll';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { Link } from 'react-router-dom';
 
 import './navbar.scss';
 
@@ -7,27 +9,24 @@ const NavBar = () => {
   return (
     <div className='navbar__container'>
       <div className='navbar__link'>
-        <Link 
-          activeClass='navbar__link'
-          to='about'
-          spy={false}
-          smooth={true}
-          offset={0}
-          duration={1000}
-        >
-          About
+        <AnchorLink href='#about'>About</AnchorLink>
+      </div>
+      <div className='navbar__link'>
+      <AnchorLink href='#experience'>Experience</AnchorLink>
+      </div>
+      <div className='navbar__link'>
+        <Link to='/listen'>
+          Listening
         </Link>
       </div>
       <div className='navbar__link'>
-        <Link 
-          activeClass='navbar__link'
-          to='experience'
-          spy={false}
-          smooth={true}
-          offset={0}
-          duration={1000}
-        >
-          Experience
+        <Link to='/read'>
+          Reading
+        </Link>
+      </div>
+      <div className='navbar__link'>
+        <Link to='/learn'>
+          Learning
         </Link>
       </div>
     </div>
