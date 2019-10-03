716fb7269faf192497110a72699ca3c7bf4d52a7
Kasaract <garynguyen2018@gmail.com>
2019-10-03 03:40:48 -0400

    Updates

---
+++ precache-manifest.e8b119ba8bd1d9f825c9bac4c804109b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac45426d9bff56978bc3c03bff12227",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df764ed6a652a86221c6",
+    "url": "/static/css/main.8696d094.chunk.css"
+  },
+  {
+    "revision": "14bf6958e7b674082cf3",
+    "url": "/static/js/2.b2bfebc3.chunk.js"
+  },
+  {
+    "revision": "df764ed6a652a86221c6",
+    "url": "/static/js/main.fbc0869d.chunk.js"
+  },
+  {
+    "revision": "679fd54d8a8c2a83dddb",
+    "url": "/static/js/runtime-main.d3268165.js"
+  },
+  {
+    "revision": "290acccdcf138f7668a1c74415f6417a",
+    "url": "/static/media/NGUYEN_GARY_resume.290acccd.pdf"
+  },
+  {
+    "revision": "ad64b847a388ae84e42c9e7e015d0906",
+    "url": "/static/media/about.ad64b847.jpg"
+  },
+  {
+    "revision": "70afa4d46310ce7892954abf689c728c",
+    "url": "/static/media/facebook.70afa4d4.svg"
+  },
+  {
+    "revision": "ab6643870117f689cedc6b215e56357c",
+    "url": "/static/media/gary3.ab664387.jpg"
+  },
+  {
+    "revision": "e5af0bc2888e6d195aa8c07517e7c5ec",
+    "url": "/static/media/github.e5af0bc2.svg"
+  },
+  {
+    "revision": "f88b2096c08be64882eba4462f9a2ab9",
+    "url": "/static/media/instagram.f88b2096.svg"
+  },
+  {
+    "revision": "1a1c953d8cd4662bcc19c44fc222d27b",
+    "url": "/static/media/linkedin.1a1c953d.svg"
+  },
+  {
+    "revision": "1477801483fdb9be8ebad8f8f44dc32d",
+    "url": "/static/media/twitter.14778014.svg"
+  }
+]);
