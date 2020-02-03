d9525c91eb707e68a808863ebb18a61b496e14f3
Kasaract <garynguyen2018@gmail.com>
2020-02-03 17:44:28 -0500

    Add splash text and navbar

--- src/Home/index.js
+++ src/Home/index.js
@@ -6,9 +6,23 @@ import './Home.scss';
 const Home = () => {
   return (
     <div className="" style={{ height: '100vh', overflow: 'hidden' }}>
-      <div className="position-relative background w-100">
-        Hello
+      <div className="position-absolute background w-100" style={{ zIndex: '-1' }}>
         <div className="layer" />
+      </div>
+
+      <div class="d-flex justify-content-end align-items-center p-3 px-md-4 ">
+        <nav class="my-2 my-md-0 mr-md-3">
+          <a class="p-3 text-white" href="#">Home</a>
+          <a class="p-3 text-white" href="#">About</a>
+          <a class="p-3 text-white" href="#">Projects</a>
+          <a class="p-3 text-white" href="#">Design</a>
+          <a class="p-3 text-white" href="#">Photography</a>
+          <a class="p-3 text-white" href="#">Contact</a>
+        </nav>
+      </div>
+      <div className="">
+        <h1 className="text-white text-center">Gary Nguyen</h1>
+        <h6 className="text-white text-center">Aspiring front-end developer, UI/UX designer, and photographer</h6>
       </div>
     </div>
   )
