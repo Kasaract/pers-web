6e73ea7e9eb4d4aebd285a48fb0dea4f0bd6f4f2
Kasaract <garynguyen2018@gmail.com>
2020-02-03 20:35:36 -0500

    Add social links

--- src/About/index.js
+++ src/About/index.js
@@ -9,33 +9,105 @@ import about from '../share/images/about.jpg';
 import './About.scss';
 
 const About = () => {
-  return (
-    <div className="my-3 pb-3">
-      <h1 className="text-center mb-3 mt-5">About Me</h1>
-      <div className="w-100 d-flex justify-content-center">
-        <img src={about} className="rounded-circle mx-auto" alt="about" style={{ maxHeight: '15rem' }} />
-      </div>
-      <div className="row my-3">
-        <div className="col" />
-        <div className="col-2 d-flex flex-row justify-content-between">
-          <img src={fb} style={{ maxHeight: '1.3rem' }} alt="fb" />
-          <img src={linkedin} style={{ maxHeight: '1.3rem' }} alt="linkedin" />
-          <img src={ig} style={{ maxHeight: '1.3rem' }} alt="instagram" />
-          <img src={github} style={{ maxHeight: '1.3rem' }} alt="github" />
-          <img src={twitter} style={{ maxHeight: '1.3rem' }} alt="twitter" />
-        </div>
-        <div className="col" />
-      </div>
-      <div className="row">
-        <div className="col" />
-        <div className="col-5">
-          <div className="text-center my-3" style={{ fontSize: '1.3rem' }}>Hey! I'm Gary. Thanks for visiting <span role="img" aria-label="(waving hand)">👋</span>. I'm a sophomore currently studying CS, Econ, and Data Science (6-14) at MIT.</div>
-          <div className="text-center my-3" style={{ fontSize: '1.3rem' }}>Looking to get in touch? Feel free to shoot me an email at <a href="mailto:garynguyen2018@gmail.com">garynguyen2018@gmail.com</a> or take a look at my <a href="#" target="_blank" rel="noopener noreferrer">resume</a>.</div>
-        </div>
-        <div className="col" />
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="my-3 pb-3">
+			<h1 className="text-center mb-3 mt-5">About Me</h1>
+			<div className="w-100 d-flex justify-content-center">
+				<img
+					src={about}
+					className="rounded-circle mx-auto"
+					alt="about"
+					style={{ maxHeight: '15rem' }}
+				/>
+			</div>
+			<div className="row my-3">
+				<div className="col" />
+				<div className="col-2 d-flex flex-row justify-content-between">
+					<a
+						href="https://www.facebook.com/profile.php?id=100017551746096"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img
+							src={fb}
+							style={{ height: '1.3rem', width: '1.3rem' }}
+							alt="fb"
+						/>
+					</a>
+					<a
+						href="https://www.linkedin.com/in/gary-nguyen-271504155/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img
+							src={linkedin}
+							style={{ height: '1.3rem', width: '1.3rem' }}
+							alt="linkedin"
+						/>
+					</a>
+					<a
+						href="https://www.instagram.com/garynguyen18/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img
+							src={ig}
+							style={{ height: '1.3rem', width: '1.3rem' }}
+							alt="instagram"
+						/>
+					</a>
+					<a
+						href="https://github.com/Kasaract"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img
+							src={github}
+							style={{ height: '1.3rem', width: '1.3rem' }}
+							alt="github"
+						/>
+					</a>
+					<a
+						href="https://twitter.com/garynguyen_18"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img
+							src={twitter}
+							style={{ height: '1.3rem', width: '1.3rem' }}
+							alt="twitter"
+						/>
+					</a>
+				</div>
+				<div className="col" />
+			</div>
+			<div className="row">
+				<div className="col" />
+				<div className="col-5">
+					<div className="text-center my-3" style={{ fontSize: '1.3rem' }}>
+						Hey! I'm Gary. Thanks for visiting{' '}
+						<span role="img" aria-label="(waving hand)">
+							👋
+						</span>
+						. I'm a sophomore currently studying CS, Econ, and Data Science
+						(6-14) at MIT.
+					</div>
+					<div className="text-center my-3" style={{ fontSize: '1.3rem' }}>
+						Looking to get in touch? Feel free to shoot me an email at{' '}
+						<a href="mailto:garynguyen2018@gmail.com">
+							garynguyen2018@gmail.com
+						</a>{' '}
+						or take a look at my{' '}
+						<a href="#" target="_blank" rel="noopener noreferrer">
+							resume
+						</a>
+						.
+					</div>
+				</div>
+				<div className="col" />
+			</div>
+		</div>
+	);
+};
 
 export default About;
--- src/Contact/index.js
+++ src/Contact/index.js
@@ -8,20 +8,73 @@ import twitter from '../share/images/social-media-icons/twitter.svg';
 import './Contact.scss';
 
 const Contact = () => (
-  <div style={{ height: '100vh' }} className="d-flex flex-column justify-content-center">
-    <h1 className="text-center my-3">Let's get in touch!</h1>
-    <div className="row my-3">
-      <div className="col" />
-      <div className="col-3 d-flex flex-row justify-content-between">
-        <img src={fb} style={{ maxHeight: '2.2rem' }} alt="fb" />
-        <img src={linkedin} style={{ maxHeight: '2.2rem' }} alt="linkedin" />
-        <img src={ig} style={{ maxHeight: '2.2rem' }} alt="instagram" />
-        <img src={github} style={{ maxHeight: '2.2rem' }} alt="github" />
-        <img src={twitter} style={{ maxHeight: '2.2rem' }} alt="twitter" />
-      </div>
-      <div className="col" />
-    </div>
-  </div>
+	<div
+		style={{ height: '100vh' }}
+		className="d-flex flex-column justify-content-center"
+	>
+		<h1 className="text-center my-3">Let's get in touch!</h1>
+		<div className="row my-3">
+			<div className="col" />
+			<div className="col-3 d-flex flex-row justify-content-between">
+				<a
+					href="https://www.facebook.com/profile.php?id=100017551746096"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={fb}
+						style={{ height: '2.2rem', width: '2.2rem' }}
+						alt="fb"
+					/>
+				</a>
+				<a
+					href="https://www.linkedin.com/in/gary-nguyen-271504155/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={linkedin}
+						style={{ height: '2.2rem', width: '2.2rem' }}
+						alt="linkedin"
+					/>
+				</a>
+				<a
+					href="https://www.instagram.com/garynguyen18/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={ig}
+						style={{ height: '2.2rem', width: '2.2rem' }}
+						alt="instagram"
+					/>
+				</a>
+				<a
+					href="https://github.com/Kasaract"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={github}
+						style={{ height: '2.2rem', width: '2.2rem' }}
+						alt="github"
+					/>
+				</a>
+				<a
+					href="https://twitter.com/garynguyen_18"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={twitter}
+						style={{ height: '2.2rem', width: '2.2rem' }}
+						alt="twitter"
+					/>
+				</a>
+			</div>
+			<div className="col" />
+		</div>
+	</div>
 );
 
 export default Contact;
