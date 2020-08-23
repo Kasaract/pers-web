88d9261d23b41867bce8c04c13ca2bb988aabc31
Gary Nguyen <garynguyen@emergentspace.com>
2020-08-23 17:35:19 -0500

    Add fade animation to misc

--- src/App.js
+++ src/App.js
@@ -33,7 +33,12 @@ const App = () => {
 					</div>
 				</div>
 				<div id="menu" class="navsign my-auto">
-					<div class="hamburger">
+					<div
+						class="hamburger"
+						data-aos="fade-right"
+						data-aos-delay="500"
+						data-aos-duration="800"
+					>
 						<p class="bar bar1"></p>
 						<p class="bar bar2"></p>
 						<p class="bar bar3"></p>
@@ -44,7 +49,12 @@ const App = () => {
 			<Experiences />
 			<Projects />
 			<Design />
-			<div className="social-container">
+			<div
+				className="social-container"
+				data-aos="fade-up"
+				data-aos-delay="600"
+				data-aos-duration="800"
+			>
 				<div className="social-list">
 					<ul className="social-item-list">
 						<li className="social-item">
@@ -94,7 +104,12 @@ const App = () => {
 					</ul>
 				</div>
 			</div>
-			<div className="email-container">
+			<div
+				className="email-container"
+				data-aos="fade-up"
+				data-aos-delay="500"
+				data-aos-duration="800"
+			>
 				<div className="email-wrapper">
 					<a className="email-link" href="mailto:nguyeng@mit.edu">
 						nguyeng@mit.edu
